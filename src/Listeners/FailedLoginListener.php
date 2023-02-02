2b3c853167ece283da954990c12dc30f6e97d898
lrljoe <104938042+lrljoe@users.noreply.github.com>
2023-02-02 07:12:22 +0000

    Update FailedLoginListener.php

--- src/Listeners/FailedLoginListener.php
+++ src/Listeners/FailedLoginListener.php
@@ -22,13 +22,22 @@ class FailedLoginListener
             return;
         }
 
+        $ip = (config('authentication-log.use-client-header')) ? request()->header(config('authentication-log.client-header-ip')) : request()->ip();
+
+        $locationString = (config('authentication-log.notifications.failed-login.headerlocation')) ?
+            ["City" => request()->header(config('authentication-log.client-header-city')),
+             "Country" => request()->header(config('authentication-log.client-header-country'))] : ((config('authentication-log.notifications.failed-login.location')) ? optional(geoip()->getLocation($ip))->toArray() : null);
+
+
+        //config('authentication-log.notifications.new-device.location') ? optional(geoip()->getLocation($ip))->toArray() : null,
         if ($event->user) {
+            //$ip = request()->header('X-Forwarded-For');
             $log = $event->user->authentications()->create([
-                'ip_address' => $ip = $this->request->ip(),
+                'ip_address' => $ip,
                 'user_agent' => $this->request->userAgent(),
                 'login_at' => now(),
                 'login_successful' => false,
-                'location' => config('authentication-log.notifications.new-device.location') ? optional(geoip()->getLocation($ip))->toArray() : null,
+                'location' => $locationString,
             ]);
 
             if (config('authentication-log.notifications.failed-login.enabled')) {
